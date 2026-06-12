4d2aaa9bddf6f4bad3f9dcf762829c0af4cbff86
Shivansh Kumar <codes.stalker@gmail.com>
2026-06-12 23:07:30 +0530

    class to interface, added methods

--- src/main/java/com/carPooling/backend/service/UploadService.java
+++ src/main/java/com/carPooling/backend/service/UploadService.java
@@ -1,4 +1,12 @@
 package com.carPooling.backend.service;
 
-public class UploadService {
+public interface UploadService {
+    void uploadImage(String base64);
+
+    /**
+     *
+     * @param base64
+     * upload doc like pdf, docx etc
+     */
+    void uploadDocuments(String base64);
 }
